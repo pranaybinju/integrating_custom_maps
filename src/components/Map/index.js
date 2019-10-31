0c2300c08d7cccba805989102fbf73f5f9d1ae5e
PKB2810 <pranay@kiprosh.com>
2019-10-31 17:08:16 +0530

    added geolocation related code

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, PermissionsAndroid, Alert, Platform} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {mapStyle} from '../../constants/mapStyle';
 export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+    };
+  }
+
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      error => {
+        Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+      },
+    );
+  }
   render() {
     return (
       <View style={{flex: 1}}>
@@ -10,12 +38,18 @@ export default class Map extends React.Component {
           provider={PROVIDER_GOOGLE}
           customMapStyle={mapStyle}
           style={{flex: 1}}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
-          }}></MapView>
+          }}>
+          <Marker
+            coordinate={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+            }}></Marker>
+        </MapView>
       </View>
     );
   }
